e72d7e412bd12026d780095047a24b7f923c1165
m-ashfaq <73794917+m-ashfaq@users.noreply.github.com>
2021-04-29 01:53:02 +1000

    checkout completed

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import data from "./data.js";
 import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/orderRouter.js";
 import userRouter from "./routers/userRouter.js";
 
 dotenv.config();
@@ -30,6 +31,10 @@ app.get("/api/products", (req, res) => {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
